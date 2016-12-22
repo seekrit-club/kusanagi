7dc2524422663a16dc6fe2c2837959f49baaaca2
chameleon <japanoise@users.noreply.github.com>
2016-12-22 11:58:29 +0100

    add test for black double push

--- move_test.go
+++ move_test.go
@@ -47,7 +47,7 @@ func TestMakeMovePawnPush(t *testing.T) {
 	}
 }
 
-func TestMakeMovePawnDoublePush(t *testing.T) {
+func TestMakeMoveWhitePawnDoublePush(t *testing.T) {
 	board, err := Parse(START)
 	if err != nil {
 		t.FailNow()
@@ -58,6 +58,22 @@ func TestMakeMovePawnDoublePush(t *testing.T) {
 	MakeMove(board, &move)
 	if GetPiece(board.Data[from]) != EMPTY || GetPiece(board.Data[to]) !=
 		PAWN || board.EnPassant != CartesianToIndex(0, 2) {
+		t.Fail()
+	}
+}
+
+func TestMakeMoveBlackPawnDoublePush(t *testing.T) {
+	board, err := Parse(START)
+	if err != nil {
+		t.FailNow()
+	}
+	board.ToMove = BLACK
+	to := byte(CartesianToIndex(0, 4))
+	from := byte(CartesianToIndex(0, 6))
+	move := Move{from, to, MoveDoublePush, EMPTY, 0}
+	MakeMove(board, &move)
+	if GetPiece(board.Data[from]) != EMPTY || GetPiece(board.Data[to]) !=
+		PAWN || board.EnPassant != CartesianToIndex(0, 5) {
 		t.Fail()
 	}
 }
