f6d7d47d8111265db3918ebfb4362fe15ce4ea46
japanoise <japanoise@tfwno.gf>
2016-12-23 19:15:05 +0000

    add tests for movegen

--- move_test.go
+++ move_test.go
@@ -78,3 +78,83 @@ func TestMakeMoveBlackPawnCapture(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func IsMoveInMoveList(t *testing.T, list []Move, from, to, kind byte) bool {
+	for _, m := range list {
+		if m.To == to && m.From == from && m.Kind == kind {
+			return true
+		}
+	}
+	return false
+}
+
+func TestMoveGenWhitePawnPush(t *testing.T) {
+	board, err := Parse(START)
+	if err != nil {
+		t.FailNow()
+	}
+	moves := MoveGen(board)
+	if !IsMoveInMoveList(t, moves, byte(CartesianToIndex(0, 1)), byte(CartesianToIndex(0, 2)), MoveQuiet) {
+		t.Fail()
+	}
+}
+
+func TestMoveGenBlackPawnPush(t *testing.T) {
+	board, err := Parse(START)
+	if err != nil {
+		t.FailNow()
+	}
+	board.ToMove = BLACK
+	moves := MoveGen(board)
+	if !IsMoveInMoveList(t, moves, byte(CartesianToIndex(0, 6)), byte(CartesianToIndex(0, 5)), MoveQuiet) {
+		t.Fail()
+	}
+}
+
+func TestMoveGenWhitePawnDoublePush(t *testing.T) {
+	board, err := Parse(START)
+	if err != nil {
+		t.FailNow()
+	}
+	moves := MoveGen(board)
+	if !IsMoveInMoveList(t, moves, byte(CartesianToIndex(0, 1)), byte(CartesianToIndex(0, 3)), MoveDoublePush) {
+		t.Fail()
+	}
+}
+
+func TestMoveGenBlackPawnDoublePush(t *testing.T) {
+	board, err := Parse(START)
+	if err != nil {
+		t.FailNow()
+	}
+	board.ToMove = BLACK
+	moves := MoveGen(board)
+	if !IsMoveInMoveList(t, moves, byte(CartesianToIndex(0, 6)), byte(CartesianToIndex(0, 4)), MoveDoublePush) {
+		t.Fail()
+	}
+}
+
+func TestMoveGenSlider(t *testing.T) {
+	board, err := Parse("4k3/8/8/1r6/8/8/8/4K3 b - - 0 1")
+	if err != nil {
+		t.FailNow()
+	}
+	moves := MoveGen(board)
+	if !IsMoveInMoveList(t, moves, byte(CartesianToIndex(1, 4)), byte(CartesianToIndex(1, 7)), MoveQuiet) {
+		t.Fail()
+	}
+}
+
+func TestMoveGenNonSlider(t *testing.T) {
+	board, err := Parse("4k3/8/8/1r6/8/8/8/4K3 b - - 0 1")
+	if err != nil {
+		t.FailNow()
+	}
+	moves := MoveGen(board)
+	if !IsMoveInMoveList(t, moves, byte(CartesianToIndex(4, 7)), byte(CartesianToIndex(4, 6)), MoveQuiet) {
+		t.Fail()
+	}
+	if IsMoveInMoveList(t, moves, byte(CartesianToIndex(4, 7)), byte(CartesianToIndex(4, 3)), MoveQuiet) {
+		t.Fail()
+	}
+}
